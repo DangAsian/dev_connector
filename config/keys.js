56e3633ab72e2838d774b97c8886d02db91f266d
Daniel Ang <dan.justin.ang@gmail.com>
2019-04-23 15:50:02 -0400

    connected mongoose db

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://abc1234:abc1234@cluster-api-fukqq.mongodb.net/test?retryWrites=true";
+  mongoURI: "mongodb+srv://abc1234:abc1234@cluster-api-fukqq.mongodb.net/test?retryWrites=true",
 }
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const app = express();
 
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("HELLO");
@@ -12,5 +22,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
